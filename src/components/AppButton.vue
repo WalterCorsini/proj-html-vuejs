0adb9aa850a3cf8152d251a60d6cd3c689c58ccc
WalterCorsini <corsini.walter@gmail.com>
2024-05-13 19:11:01 +0200

    add static hero

---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <a @click.prevent href="">
+            {{ title }}
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    background-color: blue;
+    padding: 10px 20px;
+    margin-bottom: 10px;
+    text-align: center;
+    transition: 1s;
+
+    &:hover {
+        transform: scale(1.1);
+        border-radius: 20px;
+        transition: 1s;
+    }
+
+    a {
+        color: white;
+    }
+}
+</style>
