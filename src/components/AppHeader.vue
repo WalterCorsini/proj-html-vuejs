7fc07020923246ac04f714f5398da1ca72e44472
WalterCorsini <corsini.walter@gmail.com>
2024-05-13 16:34:46 +0200

    add project scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    data(){
+        return{
+            menu: [
+        {
+          title: "Home",
+          routeName: "home",
+        },
+        {
+          title: "Chi siamo",
+          routeName: "about",
+        },
+        {
+          title: "Contatti",
+          routeName: "contacts",
+        },
+      ],
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from ".pages/HomePage.vue";
+import AboutPage from ".pages/AboutPage.vue";
+import ContactPage from ".pages/ContactPage.vue";
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: "/",
-            name: "",
-            component: NomeComponente,
+            name: "home",
+            component: HomePage,
+        },
+        {
+            path: "/chi-siamo",
+            name: "about",
+            component: AboutPage,
+        },
+        {
+            path: "/contatti",
+            name: "contacts",
+            component: ContactPage,
         }
     ],
 });
