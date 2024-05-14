ae27a1264208a3efeec89ec9ef03323b9a1fed19
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-14 13:06:05 +0200

    add: form in Contact

--- src/components/ContactOfficeAndForm.vue
+++ src/components/ContactOfficeAndForm.vue
@@ -60,20 +60,28 @@ export default {
                     </div>
                 </div>
             </div>
-            <!-- form -->
-            <div class="col">
-                <form class="row g-3">
+            <div class="col border p-0">
+                <!-- form title -->
+                <div class="border-bottom p-4">
+                    <p class="m-0">GET IN TOUCH</p>
+                </div>
+                <!-- form -->
+                <form class="row g-3 p-4">
                     <div class="col-md-6">
                         <label for="name" class="form-label">Name</label>
-                        <input type="email" class="form-control" id="name">
+                        <input type="email" class="form-control border border-0 bg-body-tertiary" id="name">
                     </div>
                     <div class="col-md-6">
                         <label for="email" class="form-label">Email</label>
-                        <input type="password" class="form-control" id="email">
+                        <input type="password" class="form-control border border-0 bg-body-tertiary" id="email">
                     </div>
                     <div class="col-12">
                         <label for="website" class="form-label">Website</label>
-                        <input type="text" class="form-control" id="website">
+                        <input type="text" class="form-control border border-0 bg-body-tertiary" id="website">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control border border-0 bg-body-tertiary" id="message" rows="3"></textarea>
                     </div>
                     <div class="col-12">
                         <button type="submit" class="btn btn-primary">Sign in</button>
@@ -89,6 +97,10 @@ export default {
 a {
     text-decoration: none;
     color: inherit;
+}
+
+input {
+    outline: none;
 }
 
 ul {
