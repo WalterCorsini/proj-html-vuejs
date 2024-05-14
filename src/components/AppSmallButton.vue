d3ae08b15c16608792a833051bb9877b4624dd5c
WalterCorsini <corsini.walter@gmail.com>
2024-05-14 18:48:57 +0200

    add static table

---
+++ src/components/AppSmallButton.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <a @click.prevent href="">
+            {{ title }}
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    background-color: #40C4FF;
+    font-size: 10px;
+    text-align: center;
+    transition: 1s;
+
+    &:hover {
+        background-color: #00aeff;
+    }
+
+    a {
+        text-decoration: none;
+        font-weight: bold;
+        font-family: sans-serif;
+        color: white;
+    }
+}
+</style>
--- src/components/HomePagePricing.vue
+++ src/components/HomePagePricing.vue
@@ -1,114 +1,180 @@
 <script>
-export default{
-    data(){
-        return{
-            arrayTitolo:[
+export default {
+    data() {
+        return {
+            arrayTitolo: [
                 {
-                        img:"",
-                        nome:"un titolo solamente",
-                        prezzo:"",
+                    img: "",
+                    nome: "un titolo solamente",
+                    prezzo: "",
                 },
                 {
-                        img:"1",
-                        nome:"Standard",
-                        prezzo:"$12",
+                    img: "1",
+                    nome: "Standard",
+                    prezzo: "$12",
                 },
                 {
-                        img:"2",
-                        nome:"Professional",
-                        prezzo:"$59",
+                    img: "2",
+                    nome: "Professional",
+                    prezzo: "$59",
                 },
                 {
-                        img:"3",
-                        nome:"Advanced",
-                        prezzo:"$88",
-                },    
+                    img: "3",
+                    nome: "Advanced",
+                    prezzo: "$88",
+                },
             ],
-            arrayTabella:[
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-                {
-                    nome:"prova",
-                    standard:"2",
-                    professional:"3",
-                    advanced:"5",
-                },
-            ]
         }
     },
-    methods:{
+    methods: {
         getImage(elem) {
             return new URL(`../assets/img/home-page/sezione-8/${elem}.jpg`, import.meta.url).href;
-        }, 
+        },
     }
 }
 </script>
 
 <template>
-<div class="container d-flex">
-    <!-- inserisci sfondo -->
-
-    <!-- titolo tabella -->
-    <div class="table d-flex flex-column align-items-center justify-content-center" v-for="curTitle in arrayTitolo">
-        <span>{{ curTitle.img }}</span>
-        <span>{{ curTitle.nome }}</span>
-        <span>{{ curTitle.prezzo }}</span>
-    </div>
-    <!-- /titolo tabella -->
-
-    <!-- tabella -->
-    <div class="table" v-for="curElem in arrayTabella">
-        <span>curElem</span>
-    </div>
     <!-- /tabella -->
-
-
-</div>
+    <div class="container">
+        <table class="table table-bordered w-100 text-center">
+            <thead>
+                <tr>
+                    <th class="cell-elem p-4" scope="col">Save Up to 40% by paying weekly</th>
+                    <th class="bg-lightblue" scope="col">
+                        <div class="d-flex flex-column align-items-center">
+                        <img src="../assets/img/home-page/sezione-9/1.png" alt="">
+                        <span>Standard</span>
+                        <span>$12</span>
+                        </div>
+                    </th>
+                    <th class="professional bg-lightblue" scope="col">
+                        <div class="d-flex flex-column align-items-center">
+                        <img src="../assets/img/home-page/sezione-9/2.png" alt="">
+                        <span>Professional</span>
+                        <span>$59</span>
+                        </div>
+                    </th>
+                    <th class="bg-lightblue" scope="col">
+                        <div class="d-flex flex-column align-items-center">
+                        <img src="../assets/img/home-page/sezione-9/3.png" alt="">
+                        <span>Advanced</span>
+                        <span>$88</span>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td scope="row">Number of Courses</td>
+                    <td>2</td>
+                    <td>4</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td scope="row">Time</td>
+                    <td>15 Days</td>
+                    <td>30 Days</td>
+                    <td>30 Days</td>
+                </tr>
+                <tr>
+                    <td scope="row">Web Designin</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row">Human-Centered-Design</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row">Basic Marketing</td>
+                    <td>&cross;</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row">Python for Everybody</td>
+                    <td>&cross;</td>
+                    <td class="text-blue">&check;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row">Android Developer</td>
+                    <td>&cross;</td>
+                    <td>&cross;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row">Business English</td>
+                    <td>&cross;</td>
+                    <td>&cross;</td>
+                    <td class="text-blue">&check;</td>
+                </tr>
+                <tr>
+                    <td scope="row"></td>
+                    <td class="btn-">
+                        <button type="button" class="btn btn-outline-info">GET IT NOW</button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-info">GET IT NOW</button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-outline-info">GET IT NOW</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-.table{
+.cell-elem {
     width: calc(100% / 4);
+}
+
+li {
+    height: 60px;
+    padding: 10px;
     border: 1px solid black;
+    text-align: center;
+}
+td{
+    color: #808080;
+}
+th{
+    border-top: 3px solid #D6F2FF;
+}
+th.professional{
+    border-top: 3px solid  #17B7FE;
+    
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+}
+.text-blue{
+    color: #17B7FE;
+}
+.btn-info{
+    color: white;
+    background-color:#17B7FE;
+    &:hover{
+        background-color: #17a2dd;
+    }    
+}
+.btn-outline-info{
+    color: #808080;
+    border-color: #17B7FE;
+    &:hover{
+        background-color:#17B7FE;
+        color: white;
+    }
+}
+.bg-lightblue{
+    background-color:#F7FDFF;
 }
 </style>
