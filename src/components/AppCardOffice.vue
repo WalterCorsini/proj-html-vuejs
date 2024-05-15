0577cbd1f0198bc26320e75bf91b81252c0671c0
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-15 16:27:13 +0200

    add: style to contact details and form

--- src/components/AppCardOffice.vue
+++ src/components/AppCardOffice.vue
@@ -13,31 +13,34 @@ export default {
 
 <template>
     <div v-for="(office, index) in propCard">
-        <h4>{{ office.city }}</h4>
+        <h6 class="title-font py-3">{{ office.city }}</h6>
         <div>
-            <ul>
-                <li>
+            <ul class="ms_contacts">
+                <li class="py-2">
                     <i class="fa-solid fa-phone"></i>
                     <span class="mx-3">{{ office.number }}</span>
                 </li>
-                <li>
+                <li class="py-2">
                     <i class="fa-regular fa-comment"></i>
                     <span class="mx-3">{{ office.email }}</span>
                 </li>
-                <li>
+                <li class="py-2">
                     <i class="fa-solid fa-building"></i>
                     <span class="mx-3">{{ office.address }}</span>
                 </li>
             </ul>
         </div>
 
-        <hr v-if="index < propCard.length - 1">
+        <hr v-if="index < propCard.length - 1" class="w-75">
     </div>
 
 </template>
 
 <style scoped lang="scss">
-ul {
-    padding: 0;
+@use "../style/partials/variables" as *;
+
+.ms_contacts {
+    color: #999999;
+    font-size: 12px;
 }
 </style>
--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -9,29 +9,33 @@ export default {
 </script>
 
 <template>
-    <form class="row g-3 p-4">
+    <form class="row g-3 p-3">
         <div class="col-6">
-            <label for="name" class="form-label">Name</label>
-            <input type="email" class="form-control border-0 bg-body-tertiary shadow-none" required id="name">
+            <label for="name" class="form-label ms_form-title">Name</label>
+            <input type="email" class="form-control border rounded-0 bg-body-tertiary shadow-none" required id="name">
         </div>
         <div class="col-6">
-            <label for="email" class="form-label">Email</label>
-            <input type="password" class="form-control border border-0 bg-body-tertiary shadow-none" required id="email">
+            <label for="email" class="form-label ms_form-title">Email</label>
+            <input type="password" class="form-control border rounded-0 bg-body-tertiary shadow-none" required id="email">
         </div>
         <div class="col-12">
-            <label for="website" class="form-label">Website</label>
-            <input type="text" class="form-control border border-0 bg-body-tertiary shadow-none" required id="website">
+            <label for="website" class="form-label ms_form-title">Website</label>
+            <input type="text" class="form-control border rounded-0 bg-body-tertiary shadow-none" required id="website">
         </div>
         <div class="col-12 mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea class="form-control border border-0 bg-body-tertiary shadow-none" required id="message" rows="3"></textarea>
+            <label for="message" class="form-label ms_form-title">Message</label>
+            <textarea class="form-control border rounded-0 bg-body-tertiary shadow-none" required id="message" rows="3"></textarea>
         </div>
         <div class="col-12">
-            <button type="submit" class="btn btn-primary">SUBMIT</button>
+            <button type="submit" class="btn btn-primary px-5 py-3 rounded-0 fw-bold">SUBMIT</button>
         </div>
     </form>
 </template>
 
 <style scoped lang="scss">
-
+.ms_form-title {
+    font-size: 12px;
+    font-weight: 600;
+    color: #353535;
+}
 </style>
